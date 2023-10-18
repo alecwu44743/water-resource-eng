8215cd2d2b4438366a1b4211a8a2682b828e9beb
9AY <cggun1999@gmail.com>
2023-10-18 19:21:37 +0800

    Make json added

--- riversec-transformer.py
+++ riversec-transformer.py
@@ -288,7 +288,7 @@ if __name__ == "__main__":
     nonduplicated_data_path = "../river-prediction-nstc/result_non-duplicated"
     save_data_path = "../river-prediction-nstc/result_non-duplicated"
     
-    make_json(dataFilePath + csvFilePath, jsonFilePath)
+    make_json(dataFilePath + csvFilePath, jsonFilePath) #make json file
     readjson2Dict(jsonFilePath)
     
     # pos_transform(raw_data_path)
